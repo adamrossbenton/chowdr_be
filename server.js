b88e9894a842fd7d6d678efe79825ba68a73b3ad
Adam Ross Benton <adamrossbenton@gmail.com>
2021-11-17 18:13:10 -0800

    setting up soups controller

--- server.js
+++ server.js
@@ -2,12 +2,31 @@
 // Dependencies
 ////////////////////////////////////////////////
 require("dotenv").config()
-const {DB_URL, PORT} = process.env
+const {DB_URL, PORT, SECRET} = process.env
 const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
+const session = require("express-session")
+const MongoStore = require("connect-mongo")
+
+const Soup = require("./models.soup")
+const SoupsRouter = require("./controllers/soups")
 
 const app = express()
+
+////////////////////////////////////////////////
+// Middleware
+////////////////////////////////////////////////
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json())
+app.use("/soups", SoupsRouter)
+app.use(session({
+    secret: SECRET,
+    store: MongoStore.create({mongoUrl: DB_URL}),
+    saveUnitialized: true,
+    resave: false
+}))
 
 ////////////////////////////////////////////////
 // Routes
